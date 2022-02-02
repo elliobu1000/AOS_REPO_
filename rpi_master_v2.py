b216158ae765a6d6b8f6eaf297617218235233f2
Gandharthus <72447580+Gandharthus@users.noreply.github.com>
2022-02-02 23:52:51 +0100

    adding a while
    We need to test the while loop with the rasp, I guess it wont
    @Elliobu

--- rpi_master_v2.py
+++ rpi_master_v2.py
@@ -1,6 +1,8 @@
 import smbus
 import time
 
+# config variables
+delay = 0.25 
 bus = smbus.SMBus(1)
 SLAVE_ADDRESS = 0x04
 
@@ -8,18 +10,20 @@ def request_data():
         read_value = int(bus.read_byte(SLAVE_ADDRESS))
         print(read_value)
 
-command = input("1: flag 1, 2: flag 2")
 
-if command == "1":
-    bus.write_byte(SLAVE_ADDRESS, 1)
-    time.sleep(1)
-    for i in range(10):
-        time.sleep(0.25)
-        request_data()
+while True:
+    command = input("1: flag 1, 2: flag 2")
 
-elif command == "2":
-    bus.write_byte(SLAVE_ADDRESS, 2)
-    time.sleep(1)
-    for i in range(40):
-        time.sleep(0.25)
-        request_data()
+    if command == "1":
+        bus.write_byte(SLAVE_ADDRESS, 1)
+        time.sleep(1)
+        for i in range(10):
+            time.sleep(delay)
+            request_data()
+
+    elif command == "2":
+        bus.write_byte(SLAVE_ADDRESS, 2)
+        time.sleep(1)
+        for i in range(40):
+            time.sleep(delay)
+            request_data()
